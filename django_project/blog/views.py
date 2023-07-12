fbd385e1a9bada02329315fb0b49aaf28c086dc2
Arun4535 <arunkhanchandani05@gmail.com>
2023-07-13 00:47:18 +0530

    added List,view,detail,update,delete views

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,8 +4,12 @@ from django.http import HttpResponse
 from .models import Post
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView
 )
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+
 
 # Create your views here.
 # blog (app) -> templates -> blog -> templates.html
@@ -39,9 +43,9 @@ class PostListView(ListView):
     ordering=['-date_posted']
 
 class PostDetailView(DetailView):
-    model=Post #model class that we want to show data from
+    model=Post 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin,CreateView):
     model=Post #model class that we want to show data from\\
     fields = ['title','content']
 
@@ -49,7 +53,33 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+# we used login_required decorator for function, decorater cant be applied to class so LoginRequiredMixin do the same
+# UserPassesTestMixin - make sure that login user has access to post created by himself as author - for that test_func() is mandatory
 
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Post #model class that we want to show data from\\
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Post
+    success_url = '/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request , 'blog/about.html',{'title' : 'About'})
