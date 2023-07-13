918d272373eae350cd8d94145ef5c874e170aad9
Arun4535 <arunkhanchandani05@gmail.com>
2023-07-14 00:53:08 +0530

    added new route for showing user specific posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.forms.models import BaseModelForm
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import (ListView,
@@ -9,7 +9,7 @@ from django.views.generic import (ListView,
                                   DeleteView
 )
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 # blog (app) -> templates -> blog -> templates.html
@@ -41,8 +41,19 @@ class PostListView(ListView):
     template_name='blog/home.html'#template name where the list will be <app_name>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering=['-date_posted']
-    paginate_by=2
+    paginate_by=5
 
+class UserPostListView(ListView):
+    model=Post #model class that we want to show data from
+    template_name='blog/user_posts.html'#template name where the list will be <app_name>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    # ordering=['-date_posted']
+    paginate_by=5
+
+    def get_queryset(self):
+        user = get_object_or_404(User,username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+    
 class PostDetailView(DetailView):
     model=Post 
 
